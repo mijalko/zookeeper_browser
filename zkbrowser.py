a04bd3b0f08f338404f255a8fe4b2ad6ac005e72
mijalko <mijalko@gmail.com>
2016-08-07 00:53:20 +0200

    first commit

---
+++ zkbrowser.py
@@ -0,0 +1,115 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
+from flask import session
+from flask import redirect
+import collections
+import datetime
+
+from kazoo.client import KazooClient
+
+
+app = Flask(__name__)
+app.secret_key = 'my secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if "connection_string" not in session or not session["connection_string"]:
+        return redirect(url_for("connect"))
+
+    connection_string = session["connection_string"]
+
+    try:
+        zk = KazooClient(hosts=connection_string, timeout=2)
+        zk.start()
+        connected = True
+        session["connection_string"] = connection_string
+    except Exception, ex:
+        connected = False
+        connection_error = "Cannot connect to zookeeoer. {}".format(str(ex))
+        if "connection_string" in session:
+            del session["connection_string"]
+        return redirect(url_for("connect"))
+    try:
+
+        # build path element
+        path = request.args.get('path', "/")
+        path_elements = path.split("/")
+        current_path = "/"
+        path_parts = []
+        path_parts.append(("/", url_for("index", path="/")))
+
+        first = True
+        for p in path_elements:
+            if p == "":
+                continue
+            if first:
+                current_path += p
+                first = False
+            else:
+                current_path += "/" + p
+            path_parts.append((p, url_for("index", path=current_path)))
+
+        # list children
+        children = zk.get_children(path)
+        children_path = []
+        for child in children:
+            children_path.append((child, url_for("index", path=path + "/" + child)))
+
+        # current path data
+        node_data = zk.get(path)
+        node_properties = []
+        node_properties.append(("creation_transaction_id", node_data[1].creation_transaction_id))
+        node_properties.append(("last_modified_transaction_id", node_data[1].last_modified_transaction_id))
+        node_properties.append(("created", str(datetime.datetime.utcfromtimestamp(node_data[1].created))))
+        node_properties.append(("last_modified", str(datetime.datetime.utcfromtimestamp(node_data[1].last_modified))))
+        node_properties.append(("version", node_data[1].version))
+        node_properties.append(("acl_version", node_data[1].acl_version))
+        node_properties.append(("owner_session_id", node_data[1].owner_session_id))
+        node_properties.append(("data_length", node_data[1].data_length))
+        node_properties.append(("children_count", node_data[1].children_count))
+
+
+        return render_template('index.html', path_parts=path_parts, children=children_path, node_properties=node_properties, data=node_data[0])
+    finally:
+        zk.stop()
+        zk.close()
+
+
+@app.route('/connect', methods=["GET", "POST"])
+def connect():
+    connection_error = None
+    connection_string = None
+    if request.method == 'POST':
+        connection_string = request.form.get('inputConStr', None)
+        if connection_string is None:
+            connection_string = "127.0.0.1:2181"
+
+    if connection_string is not None:
+        # try to connect
+        try:
+            zk = KazooClient(hosts=connection_string, timeout=2)
+            zk.start()
+            connected = True
+            session["connection_string"] = connection_string
+        except Exception, ex:
+            connected = False
+            connection_error = "Cannot connect to zookeeoer. {}".format(str(ex))
+            if "connection_string" in session:
+                del session["connection_string"]
+
+        if connected:
+            # redirect to index
+            return redirect(url_for('index'))
+
+    return render_template('connect.html', connection_error=connection_error)
+
+
+if __name__ == '__main__':
+
+
+    app.debug = True
+    app.run(host='0.0.0.0', port=4550)
